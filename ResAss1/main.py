b2ddf34536a9e65f70f7162a4a8871f86528c2dc
Malik khalid El-Lethy <101666345+MALIKKHALED@users.noreply.github.com>
2024-03-29 13:33:24 +0200

    Update main.py

--- ResAss1/main.py
+++ ResAss1/main.py
@@ -4,7 +4,7 @@ import re
 def Eliminate_implication(expression):
     pattern = r'(¬?)([(A-Za-z)]+)\s*=>\s*(¬?)([(A-Za-z)]+)'
     expression = re.sub(pattern, r'¬\1\2 | \3\4', expression)
-    pattern = r'(¬?)([(A-Za-z)]+)\s*<=>\s*(¬?)([(A-Za-z)]+)'
+    pattern = r'(¬?)(?!\()([(A-Za-z)]+)\s*<=>\s*(¬?)([(A-Za-z)\)]+)(?=\))'
     expression = re.sub(pattern, r'(¬\1\2 | \3\4) & (\1\2 | ¬\3\4)', expression)
     return expression
 
@@ -12,8 +12,8 @@ def Eliminate_implication(expression):
 def DeMorgans_laws(expression):
     pattern1 = r'¬\((.*?)\s*&\s*(.*?)\)'
     pattern2 = r'¬\((.*?)\s*\|\s*(.*?)\)'
-    expression = re.sub(pattern1, r'(¬\1 | ¬\2)', expression)
     expression = re.sub(pattern2, r'(¬\1 & ¬\2)', expression)
+    expression = re.sub(pattern1, r'(¬\1 | ¬\2)', expression)
     return expression
 
 
@@ -95,16 +95,101 @@ def Eliminate_universal_quantifiers(expression):
     expression = re.sub(pattern, '', expression)
     return expression
 
+def cconjunctions_into_clauses(expression):
+    # Regular expression pattern to match clauses separated by '&'
+    pattern = r'\s*&\s*'
 
-def CNF_Algorithm(expression):
-    expression = Eliminate_implication(expression)
-    expression = DeMorgans_laws(expression)
-    expression = remove_double_negation(expression)
-    expression = Standardize_variable_scope(expression)
-    expression = Skolemization_for_existential_quantifiers(expression)
-    expression = prenex_form(expression)
-    expression = Eliminate_universal_quantifiers(expression)
+    # Find all parts separated by '&'
+    parts = re.split(pattern, expression)
+    expression = ''
+    clauses = []
+    for part in parts:
+        P = re.split(r'\s*\|\s*', part)
+        clauses.append(P)
+    for i in clauses:
+        expression += '{' + ', '.join(i) + "}, "
+    if expression[-2] == ',':
+        expression = expression[:-2]
+    return expression
+
+def rename_variables(expression):
+    parts = expression.split('}, ')
+    unique_vars = set()
+    new_expression = ""
+    for part in parts:
+        matches = re.findall(r'[a-z]', part)
+        matches = list(set(matches))
+        if 'f' in matches:
+            matches.remove('f')
+        new_part = part
+        for match in matches:
+            if match not in unique_vars:
+                unique_vars.add(match)
+            else:
+                new_var = get_unique_variable(unique_vars)
+                unique_vars.add(new_var)
+                new_part = new_part.replace(match, new_var)
+        new_expression += new_part + "}, "
+    new_expression = new_expression[:-3]
+    return new_expression
+
+
+
+
+def handel_extra_brackets(expression):
+    parts = expression.split(',')
+    new_parts = []
+
+    for part in parts:
+
+        open_brackets = part.count('(')
+        close_brackets = part.count(')')
+
+        # Remove extra brackets
+        while open_brackets > close_brackets:
+            part = part.replace('(', '', 1)
+            open_brackets -= 1
+        while close_brackets > open_brackets:
+            part = part.replace(')', '', 1)
+            close_brackets -= 1
+
+        new_parts.append(part.strip())
+
+    # Join the processed parts with commas and return
+    return ', '.join(new_parts)
+
+def handel_extra_spaces(expression):
+    expression = expression.replace("  ", " ")
+
+    while expression[0] == ' ':
+        expression = expression[1:]
     return expression
 
 
-print(CNF_Algorithm("∀x (¬P(x) => Q(x)) & ∀x ∃y ((S(x) | R(y)) & ∀x ∃z (S(x) <=> R(z))"))
+def CNF_Algorithm(expression):
+    print(expression, '\n')
+    expression = Eliminate_implication(expression)
+    print(expression, '\n')
+    expression = DeMorgans_laws(expression)
+    print(expression, '\n')
+    expression = remove_double_negation(expression)
+    print(expression, '\n')
+    expression = Standardize_variable_scope(expression)
+    print(expression, '\n')
+    expression = Skolemization_for_existential_quantifiers(expression)
+    print(expression, '\n')
+    expression = prenex_form(expression)
+    print(expression, '\n')
+    expression = Eliminate_universal_quantifiers(expression)
+    expression = handel_extra_spaces(expression)
+    print(expression, '\n')
+    expression = cconjunctions_into_clauses(expression)
+    expression = handel_extra_brackets(expression)
+    expression = rename_variables(expression)
+    return expression
+
+#Test case 1
+print(CNF_Algorithm("∀x (¬P(x) => Q(x)) & ∀x ∃y ((S(x) | R(y)) & ∀x ∃z (S(x) => R(z))"))
+#Test case 2
+print('\n')
+print(CNF_Algorithm("∀x ∃y (P(x) <=> Q(y)) & ∀x ∃y (Q(y) => P(x))"))
